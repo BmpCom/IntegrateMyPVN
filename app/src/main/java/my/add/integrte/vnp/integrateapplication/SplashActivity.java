a505f5f34941638af4e33d7b142de28a1e7961b2
Asha Chovatiya <ashachovatiya91@gmail.com>
2023-03-02 17:26:34 +0530

    Initial commit

---
+++ app/src/main/java/my/add/integrte/vnp/integrateapplication/SplashActivity.java
@@ -0,0 +1,17 @@
+package my.add.integrte.vnp.integrateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fooball.live.tv.myapplication.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
